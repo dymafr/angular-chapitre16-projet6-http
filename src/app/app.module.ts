4f8cd0a252ca9a350b3c87b5c72faa1646de0d5d
Quentin <quentindevial19@gmail.com>
2022-11-27 12:20:57 +0100

    fix: update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
+// modules
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+// composants
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
+import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
+import { CocktailListComponent } from './cocktail-container/cocktail-list/cocktail-list.component';
+import { SelectedDirective } from './shared/directives/selected.directive';
+import { PanierContainerComponent } from './panier-container/panier-container.component';
+import { IngredientListComponent } from './panier-container/ingredient-list/ingredient-list.component';
+import { CocktailFormComponent } from './cocktail-container/cocktail-form/cocktail-form.component';
+
+//pipes
+import { FilterPipe } from './shared/pipes/filter.pipe';
+
+//routes
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CocktailListComponent,
+    CocktailDetailsComponent,
+    CocktailContainerComponent,
+    SelectedDirective,
+    PanierContainerComponent,
+    IngredientListComponent,
+    CocktailFormComponent,
+    FilterPipe,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    RouterModule.forRoot(APP_ROUTES),
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cocktail-container/cocktail-details/cocktail-details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Cocktail } from '../../shared/interfaces/cocktail.interface';
+import { CocktailService } from '../../shared/services/cocktail.service';
+import { PanierService } from '../../shared/services/panier.service';
+
+@Component({
+  selector: 'app-cocktail-details',
+  templateUrl: './cocktail-details.component.html',
+  styleUrls: ['./cocktail-details.component.scss'],
+})
+export class CocktailDetailsComponent implements OnInit {
+  public cocktail?: Cocktail;
+
+  constructor(
+    private panierService: PanierService,
+    private cocktailService: CocktailService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      const cocktailIndex = paramMap.get('index');
+      if (cocktailIndex) {
+        this.cocktailService
+          .getCocktail(+cocktailIndex)
+          .subscribe((cocktail: Cocktail) => {
+            this.cocktail = cocktail;
+          });
+      }
+    });
+  }
+
+  public addToPanier() {
+    this.panierService.addPanier(this.cocktail!.ingredients);
+  }
+}
